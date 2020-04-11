19afeb16df78327bc3eff9eae8e38bb72d9a1683
Bertrand <bhecquet.w@free.fr>
2020-04-11 16:05:50 +0200

    update train files

---
+++ generate_train_files.py
@@ -0,0 +1,24 @@
+import glob, os
+
+local_path = os.path.dirname(os.path.abspath(__file__)) + '/'
+dataset_path = local_path + 'dataset_generated'
+
+# Percentage of images to be used for the test set
+percentage_test = 10;
+
+# Create and/or truncate train.txt and test.txt
+file_train = open('web-generated-train.txt', 'w')  
+file_test = open('web-generated-test.txt', 'w')
+
+# Populate train.txt and test.txt
+counter = 1  
+index_test = round(100 / percentage_test)  
+for pathAndFilename in glob.iglob(os.path.join(dataset_path, "*.jpg")):  
+    title, ext = os.path.splitext(os.path.basename(pathAndFilename))
+
+    if counter == index_test+1:
+        counter = 1
+        file_test.write("dataset_generated/" + title + '.jpg' + "\n")
+    else:
+        file_train.write("dataset_generated/" + title + '.jpg' + "\n")
+        counter = counter + 1
