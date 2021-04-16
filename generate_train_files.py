c3e8965b2d893558bc13a7b5df16978523cbbb1e
bhecquet <bhecquet.w@free.fr>
2021-04-16 09:31:36 +0200

    update training dataset generator

--- generate_train_files.py
+++ generate_train_files.py
@@ -1,28 +1,47 @@
+"""
+Script that will create a "dataset" folder with "images" and "labels" sub-folders
+These folders will contain pictures from "dataset_extracted" and "dataset_real" folders
+"""
 import glob, os
+import random
+import shutil
 
 local_path = os.path.dirname(os.path.abspath(__file__)) + '/'
-dataset_path = local_path + 'dataset_extracted'
+
 
 # Percentage of images to be used for the test set
 percentage_test = 10;
 max_number_of_files = 2000
+dataset_folder = local_path + 'dataset'
+dataset_training_folder = os.path.join(dataset_folder, 'training')
+dataset_testing_folder = os.path.join(dataset_folder, 'test')
+dataset_training_images_folder = os.path.join(dataset_training_folder, 'images')
+dataset_training_labels_folder = os.path.join(dataset_training_folder, 'labels')
+dataset_testing_images_folder = os.path.join(dataset_testing_folder, 'images')
+dataset_testing_labels_folder = os.path.join(dataset_testing_folder, 'labels')
 
-# Create and/or truncate train.txt and test.txt
-file_train = open('web-generated-train.txt', 'w')  
-file_test = open('web-generated-test.txt', 'w')
 
-# Populate train.txt and test.txt
-counter = 1  
-index_test = round(100 / percentage_test)  
-for loop, pathAndFilename in enumerate(glob.iglob(os.path.join(dataset_path, "*.jpg"))):  
-    title, ext = os.path.splitext(os.path.basename(pathAndFilename))
+if os.path.isdir(dataset_folder):
+    shutil.rmtree(dataset_folder, ignore_errors=True)
+os.makedirs(dataset_training_images_folder, exist_ok=True)
+os.makedirs(dataset_training_labels_folder, exist_ok=True)
+os.makedirs(dataset_testing_images_folder, exist_ok=True)
+os.makedirs(dataset_testing_labels_folder, exist_ok=True)
 
-    if counter == index_test+1:
-        counter = 1
-        file_test.write("dataset_extracted/" + title + '.jpg' + "\n")
-    else:
-        file_train.write("dataset_extracted/" + title + '.jpg' + "\n")
-        counter = counter + 1
+for dataset_path in [local_path + 'dataset_extracted', local_path + 'dataset_generated_small']:
+    
+    random.seed(2)
+    
+    counter = 1  
+    index_test = round(100 / percentage_test)  
+    for path_and_filename in glob.iglob(os.path.join(dataset_path, "*.jpg")):  
+        file_path, ext = os.path.splitext(path_and_filename)
         
-    if loop > max_number_of_files:
-        break
+        train_or_test = random.randint(0, 100)
+        if train_or_test < percentage_test:
+            shutil.copy(path_and_filename, dataset_testing_images_folder)
+            shutil.copy(file_path + '.txt', dataset_testing_labels_folder)
+        else:
+            shutil.copy(path_and_filename, dataset_training_images_folder)
+            shutil.copy(file_path + '.txt', dataset_training_labels_folder)
+    
